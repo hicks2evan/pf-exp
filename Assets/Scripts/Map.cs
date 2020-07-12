ac05f9127f8a2d25a98fc37366ab2744c3134499
Evan Hicks <hicks2evan@gmail.com>
2020-07-12 13:31:42 -0600

    getting C# code stuff set up, proving I can hit a breakpoint in debugger

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I hit a break point ???");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
